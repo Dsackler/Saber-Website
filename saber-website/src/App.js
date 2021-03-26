a4fbc334b4147acd492c2a4a0f510f3184ecf55d
David Sackler <davidsackler@Davids-MBP.home>
2021-03-25 22:44:16 -0700

    Started parallax implemtation

--- saber-website/src/App.js
+++ saber-website/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import {Parallax} from 'react-parallax';
+import carPic from "./Images/car.jpeg";
+import fishPic from "./Images/fish.jpeg";
+
+
+const iStyle = {
+  background: '#fff',
+  position: 'absolute',
+  left: '50%',
+  top: '50%',
+  padding: '20px',
+  transform: 'translate(-50%, -50%)'
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Parallax bgImage = {carPic} strength = {500}>
+        <div className = "section">
+          <div style = {iStyle}>HTML in parallax</div>
+        </div>
+      </Parallax>
+      <h1>Please help</h1>
+      <Parallax bgImage = {fishPic} blur={{ min: -15, max: 15 }}>
+        <div className = "section">
+          <div style = {iStyle}>HTML in parallax</div>
+        </div>
+      </Parallax>
+      <div style = {{height: '500vh'}}></div>
     </div>
   );
 }
